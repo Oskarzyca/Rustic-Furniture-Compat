2447c58d10676083e2f5563c1bc71b7bb02d78bf
_05K4R_ <oskaros036@gmail.com>
2023-02-14 11:55:19 +0100

    Figured out all the git stuff

---
+++ src/main/java/oskarzyca/rusticcompat/proxy/CommonProxy.java
@@ -0,0 +1,31 @@
+package oskarzyca.rusticcompat.proxy;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import oskarzyca.rusticcompat.block.ModBlocks;
+
+public class CommonProxy {
+    public static Configuration config;
+
+    public void preInit(FMLPreInitializationEvent event) {
+        ModBlocks.init();
+        if (Loader.isModLoaded("traverse")){
+            ModBlocks.traverseInit();
+        }
+        if (Loader.isModLoaded("defiledlands")){
+            ModBlocks.defiledInit();
+        }
+        if (Loader.isModLoaded("betternether")){
+            ModBlocks.betnetInit();
+        }
+    }
+
+    public void init(FMLInitializationEvent event) {
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
